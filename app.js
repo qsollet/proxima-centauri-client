3c3b61bbfb78e2db76d6d4350900fd350a85245c
qsollet <quentin@sollet.fr>
2017-01-10 21:14:05 +0000

    files are now served with nodejs

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var app = express()
+var port = 3000
+
+app.use(express.static('web'))
+
+app.get('/', function(req, res) {
+    res.sendFile('web/index.html')
+})
+
+app.listen(port, function() {
+    console.log('Server started on port '+port)
+})
